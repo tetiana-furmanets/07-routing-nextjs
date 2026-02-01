f4f8c8818060717bf066502a1cefd82203e0a4a5
Tetiana Furmanets <tetyana.best.89@ukr.net>
2026-02-01 12:18:00 +0100

    fix: correct axios interceptor headers handling

--- lib/api.ts
+++ lib/api.ts
@@ -13,12 +13,11 @@ const instance = axios.create({
 
 instance.interceptors.request.use(config => {
   const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-  if (token) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${token}`,
-    };
+
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
